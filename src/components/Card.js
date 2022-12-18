9a28f6f6e15b60c0714b769917a67dd4f104e8f9
Rhodmar <113713078+Rhodmar@users.noreply.github.com>
2022-12-19 04:23:13 +0800

    added designs

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,20 @@ import React from 'react'
 const Card = ({data, addToCart}) => {
     return (
         <div className="card" >
+            
+           <div className="test">
+            <div className="gradient">
             <img src={data.img} className="card-image" alt="product" title={data.title} />
+            </div>
+            <div className="card-info">
             <h3 className="card-title">{data.title}</h3>
-            <p className="price"><span>PHP {data.price}</span></p>
-            <button className="add-to-cart" data-testid ="addtocart" onClick={() => addToCart(data)}>Add to cart</button>
+            <p className="card-price"><span>PHP {data.price}</span></p>
+            
+            </div>
+           </div>
+           <button className="add-to-cart" data-testid ="addtocart" onClick={() => addToCart(data)}>Add to cart</button>
         </div>
+        
     )
 }
 
