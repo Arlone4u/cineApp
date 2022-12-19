e5e5185707df6720d4c4990de0286aabee785f4d
Donjie dela Victoria <donjiedvictoria@gmail.com>
2022-12-19 18:52:15 +0800

    OH SHIIIT
    added gifs on hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Card = ({data, addToCart}) => {
+    const [imageSrc, setImageSrc] = useState(data.img);
+
+    function handleMouseOver() {
+        setImageSrc(data.clip);
+      }
+      
+      function handleMouseOut() {
+        setImageSrc(data.img);
+      }
+
     return (
         <div className="card" >
             
            <div className="test">
             <div className="gradient">
-            <img src={data.img} className="card-image" alt="product" title={data.title} />
+            <img src={imageSrc} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="card-image" alt="product" title={data.title} />
             </div>
             <div className="card-info">
             <h3 className="card-title">{data.title}</h3>
